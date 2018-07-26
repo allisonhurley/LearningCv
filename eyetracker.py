e69c37a618414547d41c422bb7b94d85a2ff50bd
Allison Hurley <nolan0117@comcast.net>
2018-07-26 13:36:02 -0400

    Added the image mirroring

--- eyetracker.py
+++ eyetracker.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) 
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
+    frame = cv2.flip(frame,1)
+    
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -14,6 +15,7 @@ while(True):
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 
 # When everything done, release the capture
 cap.release()
